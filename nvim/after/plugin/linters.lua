f31c9f2bb049c2730d9f6f61e6263a8834a59e4f
Mario Gonzalez <maario@amazon.com>
2026-08-07 13:45:49 -0600

    Async Linters

--- nvim/after/plugin/linters.lua
+++ nvim/after/plugin/linters.lua
@@ -1,33 +1,61 @@
-local function find_checkstyle_config()
-    local root_dir = vim.fs.root(0, {'packageInfo', 'Config', '.git'})
-    if not root_dir then
-        return nil
-    end
+local cached_checkstyle_config = nil
 
-    local checkstyle_files = vim.fn.globpath(root_dir, '**/checkstyle.xml', false, true)
-    
-    for _, file in ipairs(checkstyle_files) do
-        if not string.match(file, '/build/') then
-            print("Using checkstyle config: " .. file)
-            return file
-        end
+local function find_checkstyle_config_async(callback)
+  if cached_checkstyle_config then
+    callback(cached_checkstyle_config)
+    return
+  end
+
+  local root_dir = vim.fs.root(0, { 'packageInfo', 'Config', '.git' })
+  if not root_dir then
+    callback(nil)
+    return
+  end
+
+  -- Pure Lua async search using vim.fs.find
+  -- It stops immediately when encountering these blacklisted directories
+  local found = vim.fs.find('checkstyle.xml', {
+    path = root_dir,
+    upward = false,
+    limit = 1,
+    stop = function(name)
+      return name == 'build' or name == 'dist' or name == 'node_modules' or name == 'target'
     end
-    
-    print("No checkstyle.xml found")
-    return nil
+  })
+
+  if #found > 0 then
+    cached_checkstyle_config = found[1]
+    callback(found[1])
+  else
+    callback(nil)
+  end
 end
 
-local checkstyle_config = find_checkstyle_config()
-
-if checkstyle_config then
-    require('lint').linters.checkstyle.args = {'-c', checkstyle_config}
-    require('lint').linters_by_ft = {java = {'checkstyle'}}
-else
-    require('lint').linters_by_ft = {}
-end
-
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost" }, {
+  pattern = "*.java", -- Only run this logic for Java files to save CPU
   callback = function()
-    require("lint").try_lint()
+    find_checkstyle_config_async(function(config_path)
+      local lint = require('lint')
+
+      local out_message = "CheckStyle File NOT FOUND using default."
+      if config_path then
+          out_message = string.format("Found CheckStyle at: %s", config_path)
+      end
+
+      vim.notify(
+        out_message,
+        vim.log.levels.INFO,
+        { title = "[CheckStyle Linters]" }
+      )
+      
+      if config_path then
+        lint.linters.checkstyle.args = { '-c', config_path }
+        lint.linters_by_ft = { java = { 'checkstyle' } }
+        -- Trigger the linter now that the configuration is ready
+        lint.try_lint()
+      else
+        lint.linters_by_ft = { java = {} }
+      end
+    end)
   end,
 })
