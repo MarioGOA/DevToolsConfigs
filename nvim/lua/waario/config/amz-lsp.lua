bd43c5ab6e1ff07ff5fa85e3f4a9802f7d062be3
Mario Gonzalez <maario@amazon.com>
2026-08-13 13:25:16 -0600

    fix(plugin-fixes): Removes a duplicated file and updates Bemol
        - Removed duplicated teminal plugin file
        - Updated amz-lsp script to use autocmd
        - Updated aerospace spaces for finder and zoom

--- nvim/lua/waario/config/amz-lsp.lua
+++ nvim/lua/waario/config/amz-lsp.lua
@@ -9,27 +9,42 @@ function isBrazilWorkspace()
     end
 end
 
-function initializeBemol()
-    if isBrazilWorkspace() then
-        print("Brazil Workplace detected...")
-    else
-        return
-    end
-
-    local root_dir = vim.fs.root(0, {'packageInfo', 'Config'})
-
-    if root_dir and vim.fn.isdirectory(root_dir .. "/.bemol") == 0 then
-        print("Bemol folder not found - running command")
-        vim.system({'bemol'}, {
-            cwd = root_dir,
-        }, function(obj)
-            if obj.code == 0 then
-                print("Bemol completed successfully")
-            else
-                print("Bemol failed: " .. (obj.stderr or "Unknown error"))
-            end
-        end)
-    end
+function notify(out_message)
+    vim.notify(
+        out_message,
+        vim.log.levels.INFO,
+        { title = "[LSP Pre-Steps]" }
+    )
 end
 
-vim.schedule(initializeBemol)
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function ()
+        local out_message = "Not in an Brazil Workplace"
+        local workplace_found = isBrazilWorkspace()
+
+        if workplace_found then
+            out_message = "Brazil Workplace detected..."
+        end
+        notify(out_message)
+
+        if not workplace_found then
+            return
+        end
+
+        local root_dir = vim.fs.root(0, {'packageInfo', 'Config'})
+
+        if root_dir and vim.fn.isdirectory(root_dir .. "/.bemol") == 0 then
+            notify("Bemol folder not found - running it")
+            vim.system({'bemol'}, {
+                cwd = root_dir,
+            }, function(obj)
+                if obj.code == 0 then
+                    notify("Bemol completed successfully")
+                else
+                    notify("Bemol failed: " .. (obj.stderr or "Unknown error"))
+                end
+            end)
+        end
+    end,
+})
+
